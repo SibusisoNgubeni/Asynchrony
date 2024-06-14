f160b782c33f472727d35de11dde07f0fb9d9af3
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-06-14 11:09:53 +0200

    documentation

--- djs10/src/components/FetchRequest.jsx
+++ djs10/src/components/FetchRequest.jsx
@@ -23,9 +23,25 @@ export default function FetchRequest() {
       });
   }, []);
 
+
+
+/**
+ * Render function
+ * @loading -way to handle the loading state gracefully and provide feedback to the user that the data is being fetched
+ * @error && .... - render error message if API not fetched
+ * @(data.map) -Maps over the data array, which contains the blog post data. and returns a list item element
+   (<li>) for each post
+ * @(post.id) -Maps over the data array, which contains the blog post data. For each post, it returns a list item element
+    (<li>) with the post title and body
+ * @(index+1) -Renders a heading element with the post title, prefixed with the index number
+ * @(post.body) -element with the post body text.
+
+
+ */
+
   return (
     <div>
-      {loading && <p>Loading...</p>}
+      {loading && <p>Loading...</p>} 
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {!error && data && (
         <>
